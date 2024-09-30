4451c1edacdb085cdddfbaa71a5b8aead6a5d0a5
Mateus Cardoso Oliveira <cardoso.olim@gmail.com>
2024-09-30 14:26:29 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'P@ssw0rd';
+    $dbName = 'formulario';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
